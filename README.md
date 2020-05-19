22876dbae26ff2a7b1f4eacf3bb0cc39ba64e8d7
Xidorn Quan <me@upsuper.org>
2020-05-19 22:42:45 +1000

    Support delegate a single method

--- README.md
+++ README.md
@@ -2,20 +2,38 @@
 
 Attribute proc-macro to delegate method to a field.
 
-# Examples
+## Examples
 
-```rust
+### Delegate `impl` block
+
+```
 use delegate_attr::delegate;
 
-struct Foo {
-    a: Vec<u32>,
+struct Foo(String);
+
+#[delegate(self.0)]
+impl Foo {
+    fn as_str(&self) -> &str;
+    fn into_bytes(self) -> Vec<u8>;
 }
 
-#[delegate(a)]
-impl Foo {
+let foo = Foo("hello".to_owned());
+assert_eq!(foo.as_str(), "hello");
+assert_eq!(foo.into_bytes(), b"hello");
+```
+
+```
+use delegate_attr::delegate;
+
+struct Foo<T> {
+    a: Vec<T>,
+}
+
+#[delegate(self.a)]
+impl<T> Foo<T> {
     fn len(&self) -> usize;
-    fn get(&self, index: usize) -> Option<&u32>;
-    fn push(&mut self, value: u32);
+    fn get(&self, index: usize) -> Option<&T>;
+    fn push(&mut self, value: T);
 }
 
 let mut foo = Foo { a: vec![1] };
@@ -23,4 +41,20 @@ assert_eq!(foo.get(0), Some(&1));
 foo.push(10);
 assert_eq!(foo.get(1), Some(&10));
 assert_eq!(foo.len(), 2);
+```
+
+### Delegate single method
+
+```
+use delegate_attr::delegate;
+
+struct Foo<T>(Vec<T>);
+
+impl<T> Foo<T> {
+    #[delegate(self.0)]
+    fn len(&self) -> usize;
+}
+
+let foo = Foo(vec![1]);
+assert_eq!(foo.len(), 1);
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 //!
 //! # Examples
 //!
+//! ## Delegate `impl` block
+//!
 //! ```
 //! use delegate_attr::delegate;
 //!
@@ -38,25 +40,44 @@
 //! assert_eq!(foo.get(1), Some(&10));
 //! assert_eq!(foo.len(), 2);
 //! ```
+//!
+//! ## Delegate single method
+//!
+//! ```
+//! use delegate_attr::delegate;
+//!
+//! struct Foo<T>(Vec<T>);
+//!
+//! impl<T> Foo<T> {
+//!     #[delegate(self.0)]
+//!     fn len(&self) -> usize;
+//! }
+//!
+//! let foo = Foo(vec![1]);
+//! assert_eq!(foo.len(), 1);
+//! ```
 
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{
-    parse_macro_input, Expr, FnArg, ImplItem, ImplItemMethod, Item, ItemImpl, Pat, Signature,
-};
+use syn::{parse_macro_input, Expr, FnArg, ImplItem, ImplItemMethod, ItemImpl, Pat, Signature};
 
 #[proc_macro_attribute]
 pub fn delegate(attr: TokenStream, item: TokenStream) -> TokenStream {
     let receiver = parse_macro_input!(attr as Expr);
-    let input = parse_macro_input!(item as Item);
-    let output = match input {
-        Item::Impl(impl_block) => derive_impl_block(impl_block, receiver),
-        _ => panic!("Expected an impl block"),
-    };
-    output.into()
+    delegate_input(item, &receiver).into()
 }
 
-fn derive_impl_block(input: ItemImpl, receiver: Expr) -> proc_macro2::TokenStream {
+fn delegate_input(input: TokenStream, receiver: &Expr) -> proc_macro2::TokenStream {
+    if let Ok(input) = syn::parse::<ItemImpl>(input.clone()) {
+        return delegate_impl_block(input, receiver);
+    }
+    if let Ok(input) = syn::parse::<ImplItemMethod>(input) {
+        return delegate_method(input, receiver);
+    }
+    panic!("Expected an impl block or method inside impl block")
+}
+
+fn delegate_impl_block(input: ItemImpl, receiver: &Expr) -> proc_macro2::TokenStream {
     let ItemImpl {
         attrs,
         defaultness,
@@ -70,46 +91,51 @@ fn derive_impl_block(input: ItemImpl, receiver: Expr) -> proc_macro2::TokenStrea
     } = input;
     let trait_ = trait_.map(|(bang, path, for_)| quote!(#bang #path #for_));
     let items = items.into_iter().map(|item| {
-        let ImplItemMethod {
-            attrs,
-            vis,
-            defaultness,
-            sig,
-            block: _,
-        } = match item {
+        let method = match item {
             ImplItem::Method(m) => m,
             _ => return item.into_token_stream(),
         };
-        let Signature {
-            ident: name,
-            inputs,
-            ..
-        } = &sig;
-        let mut inputs = inputs.into_iter();
-        assert!(
-            matches!(inputs.next(), Some(FnArg::Receiver(_))),
-            "Only methods with receiver (self) is supported",
-        );
-        let args = inputs.map(|arg| {
-            let pat = match arg {
-                FnArg::Typed(pat) => pat,
-                _ => panic!("Unexpected token"),
-            };
-            match &*pat.pat {
-                Pat::Ident(ident) => ident.to_token_stream(),
-                _ => panic!("Only identifier on argument is supported"),
-            }
-        });
-        quote! {
-            #(#attrs)* #vis #defaultness #sig {
-                #receiver.#name(#(#args),*)
-            }
-        }
+        delegate_method(method, receiver)
     });
 
     quote! {
         #(#attrs)* #defaultness #unsafety #impl_token #generics #trait_ #self_ty {
             #(#items)*
+        }
+    }
+}
+
+fn delegate_method(input: ImplItemMethod, receiver: &Expr) -> proc_macro2::TokenStream {
+    let ImplItemMethod {
+        attrs,
+        vis,
+        defaultness,
+        sig,
+        block: _,
+    } = input;
+    let Signature {
+        ident: name,
+        inputs,
+        ..
+    } = &sig;
+    let mut inputs = inputs.into_iter();
+    assert!(
+        matches!(inputs.next(), Some(FnArg::Receiver(_))),
+        "Only methods with receiver (self) is supported",
+    );
+    let args = inputs.map(|arg| {
+        let pat = match arg {
+            FnArg::Typed(pat) => pat,
+            _ => panic!("Unexpected token"),
+        };
+        match &*pat.pat {
+            Pat::Ident(ident) => ident.to_token_stream(),
+            _ => panic!("Only identifier on argument is supported"),
+        }
+    });
+    quote! {
+        #(#attrs)* #vis #defaultness #sig {
+            #receiver.#name(#(#args),*)
         }
     }
 }
